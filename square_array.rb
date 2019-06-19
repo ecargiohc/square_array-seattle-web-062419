cfc0d69de973e6cda597e43f055e949d2dc09172
Grace Colette <ilovegrace09@gmail.com>
2019-06-19 17:29:19 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,6 @@
 def square_array(array) 
-  new_array = array.collect { |numbers| numbers ** 2 }
+  new_array = []
+  array.each { |numbers| new_array << numbers ** 2 }
+  return new_array
+end
 end
